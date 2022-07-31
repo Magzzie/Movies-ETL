1a7161b3308b17b968852b89b73f85c09417ef86
Magzzie <99442686+Magzzie@users.noreply.github.com>
2022-07-31 17:09:27 -0400

    Uploading SQL db code & images & final report.

---
+++ sql_query.sql
@@ -0,0 +1,7 @@
+SELECT * FROM movies;
+SELECT * FROM ratings;
+
+-- Get the number of rows in movies table
+SELECT COUNT(*) FROM movies;
+-- Get the number of rows in movies table
+SELECT COUNT(*) FROM ratings;
